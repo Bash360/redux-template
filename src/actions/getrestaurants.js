2167ce7247582b6921f028f43cb8dcb770f86cc1
mark bashir <beejayphil@gmail.com>
2019-11-21 13:01:03 +0100

    remove example action and components

--- src/actions/getrestaurants.js
+++ src/actions/getrestaurants.js
@@ -4,7 +4,7 @@ export default function setRestaurants() {
   return function (dispatch) {
     dispatch({ type: REQUEST_RESTAURANT_PENDING });
     fetch("http://localhost:5000/api/v1/restaurants").then(data => data.json())
-      .then(data => dispatch({ type: REQUEST_RESTAURANT_SUCCESS, payload: data }))
+      .then(data => dispatch({ type: REQUEST_RESTAURANT_SUCCESS, payload: data.data }))
       .catch(error => dispatch({ type: REQUEST_RESTAURANT_FAILED, payload: error.message }));
   }
  }
